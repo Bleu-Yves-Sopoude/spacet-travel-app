17466e0f779210e6f87ecf194767ca4fb1e1d9cd
Haider <malikhaiderkhan81@gmail.com>
2023-08-15 21:17:14 +0500

    display missions data

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,15 +1,32 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchmissions } from '../redux/missions/missionsSlice';
 
 const Mission = () => {
   const dispatch = useDispatch();
+  const { missions, isLoading, error } = useSelector((state) => state.missions);
   useEffect(() => {
     dispatch(fetchmissions());
   }, [dispatch]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div>
-      <h4>Missions</h4>
+      <ul>
+        {missions.map((mission) => (
+          <li key={mission.mission_id}>
+            <h5>{mission.mission_name}</h5>
+            <p>{mission.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
