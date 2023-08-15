bacb573b27528663c1c5f6ab25d929d9d6ae5953
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-15 16:16:42 +0000

    Add

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -9,32 +9,11 @@ const Rocket = () => {
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
-
+  
+  console.log(rocketsdata)
   return (
     <div>
-      {rocketsdata.loading ? (
-        <div>Loading ...</div>
-      ) : (
-        <ul className="rocketsList">
-          {rocketsdata.data.map((e) => (
-            <>
-              <li key={e.id}>
-                {e.flickr_images && <img src={e.flickr_images} alt={e.name} />}
-                <h2>{e.name}</h2>
-                <p>
-                  {!e.reserved && (
-                    <p style={{ background: 'red', padding: '40px' }}>
-                      Reserved
-                    </p>
-                  )}
-                  {e.description}
-                </p>
-              </li>
-              <hr />
-            </>
-          ))}
-        </ul>
-      )}
+     <h4>Rocket</h4>
     </div>
   );
 };
