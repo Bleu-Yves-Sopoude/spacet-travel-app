d3a7f37c389bc91b171888cb2807a7628cd49ec0
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-15 08:51:28 +0000

    Add structure

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,15 @@
+import {createSlice } from '@reduxjs/toolkit'
+
+const initialState ={
+    rocketItems:[],
+    isLoading:true
+}
+
+const rocketsSlice = createSlice({
+    name:'rockets',
+    initialState,
+
+});
+
+export default rocketsSlice.createSlice;
+
