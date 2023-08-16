d7b80ca2dd8428442c908a14bf8d82be0ec752ec
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-16 13:18:01 +0000

    Fix linter

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,16 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   data: [],
-  status: "idle",
+  status: 'idle',
   error: null,
   loading: false,
 };
 
-export const fetchRockets = createAsyncThunk("rockets/fetchData", async () => {
+export const fetchRockets = createAsyncThunk('rockets/fetchData', async () => {
   try {
-    const response = await axios.get("https://api.spacexdata.com/v3/rockets");
+    const response = await axios.get('https://api.spacexdata.com/v3/rockets');
     const rocketsData = response.data.map((rocket) => ({
       id: rocket.id,
       name: rocket.rocket_name,
@@ -22,12 +22,12 @@ export const fetchRockets = createAsyncThunk("rockets/fetchData", async () => {
 
     return rocketsData;
   } catch (error) {
-    throw new Error("Unable to fetch data for the rockets");
+    throw new Error('Unable to fetch data for the rockets');
   }
 });
 
 const rocketsSlice = createSlice({
-  name: "rockets",
+  name: 'rockets',
   initialState,
   reducers: {
     bookRocket: (state, action) => {
@@ -43,14 +43,14 @@ const rocketsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchRockets.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchRockets.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.data = action.payload;
       })
       .addCase(fetchRockets.rejected, (state, action) => {
-        state.status = "failed";
+        state.status = 'failed';
         state.error = action.error.message;
       });
   },
