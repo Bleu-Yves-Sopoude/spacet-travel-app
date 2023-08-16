d712c62a7941bf61b5c329669b604ac158c4c30e
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-16 12:27:33 +0000

    Add Booking

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,7 +1,8 @@
-import { React, useEffect } from 'react';
-import './Rocket.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../redux/rockets/rocketsSlice';
+import { React, useEffect } from "react";
+import classes from "./Rocket.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRockets } from "../redux/rockets/rocketsSlice";
+import { bookRocket as bookrocketAction } from "../redux/rockets/rocketsSlice";
 
 const Rocket = () => {
   const rocketsdata = useSelector((state) => state.rockets);
@@ -9,7 +10,11 @@ const Rocket = () => {
 
   useEffect(() => {
     dispatch(fetchRockets());
-  }, [dispatch, rocketsdata]);
+  }, [dispatch]);
+
+  const bookRocket = (id) => {
+    dispatch(bookrocketAction(id));
+  };
 
   return (
     <div>
@@ -17,23 +22,30 @@ const Rocket = () => {
         <div>Loading ...</div>
       ) : (
         <ul className="rocketsList">
-          {rocketsdata.data.map((rocket) => (
-            <li key={rocket.id}>
-              <div className="rocketImgageContainer">
-                <img src={rocket.flickr_images} alt="" />
-              </div>
+          {rocketsdata.data.map((rocket) => {
+            return (
+              <li key={rocket.id}>
+                <div className="rocketImgageContainer">
+                  <img src={rocket.flickr_images} alt="" />
+                </div>
 
-              <div className="rocketDescription">
-                <h2>
-                  {rocket.name}
-                </h2>
-                <p>
-                  <>{rocket.reserved && <span>reserved</span>}</>
-                  {rocket.description}
-                </p>
-              </div>
-            </li>
-          ))}
+                <div className="rocketDescription">
+                  <h2>{rocket.name} </h2>
+                  <p>
+                    <>{rocket.reserved && <span>reserved</span>}</>
+                    {rocket.description}
+                  </p>
+                  <button
+                    className={rocket.reserved && classes.cancel}
+                    onClick={() => bookRocket(rocket.id)}
+                  >
+                    {" "}
+                    {rocket.reserved ? <>cancel reservation </> : <>reserve</>}
+                  </button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -5,7 +5,7 @@ const initialState = {
   data: [],
   status: 'idle',
   error: null,
-  loading: false,
+  loading: false
 };
 
 export const fetchRockets = createAsyncThunk('rockets/fetchData', async () => {
@@ -29,7 +29,18 @@ export const fetchRockets = createAsyncThunk('rockets/fetchData', async () => {
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
-  reducers: {},
+  reducers: {
+    bookRocket: (state, action) => {
+      const bookRocket = state.data.map((rocket) => {
+        if (rocket.id === action.payload) {
+          return { ...rocket, reserved: true }
+        } else {
+          return { ...rocket }
+        }
+      });
+      state.data = bookRocket
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchRockets.pending, (state) => {
@@ -47,3 +58,4 @@ const rocketsSlice = createSlice({
 });
 
 export default rocketsSlice.reducer;
+export const { bookRocket } = rocketsSlice.actions;
