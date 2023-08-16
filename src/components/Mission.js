71d8dbaee9d0a089897a3722cdbb75af7d19d566
Haider <malikhaiderkhan81@gmail.com>
2023-08-16 20:47:22 +0500

    badge added

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Table } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import { fetchmissions, joinmission } from '../redux/missions/missionsSlice';
 
 const Mission = () => {
@@ -48,7 +49,13 @@ const Mission = () => {
             <tr key={mission.mission_id}>
               <td><h5>{mission.mission_name}</h5></td>
               <td><p>{mission.description}</p></td>
-              <td><p>Coming Soon</p></td>
+              <td>
+                {mission.reserved ? (
+                  <h5><Badge bg="success">Active Member</Badge></h5>
+                ) : (
+                  <h5><Badge bg="primary">NOT A MEMBER</Badge></h5>
+                )}
+              </td>
               <td className="d-flex align-items-center"><Button variant="outline-dark" onClick={() => handlejoinmission(mission.mission_id, mission.reserved)}>{mission.reserved ? 'Leave Mission' : 'Join Mission'}</Button></td>
             </tr>
           ))}
