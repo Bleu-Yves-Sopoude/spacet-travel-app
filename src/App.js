2fe0772d7dd8b17ef35128293b2bbb82fa62747f
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-17 15:25:46 +0000

    Fix linter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-//import Navigations from './components/Navigations';
-=======
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigations from './components/Navigations';
->>>>>>> bc76d372bbb07d341f0fcb69e1d8be82bf43ae9d
 import Rocket from './components/Rocket';
 import Mission from './components/Mission';
 import Profile from './components/myProfile';
